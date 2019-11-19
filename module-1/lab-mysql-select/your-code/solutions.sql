38769836e7171b36d578f529bb5d48dfdba00f14
almsasantos <alms.asantos@gmail.com>
2019-11-19 17:45:29 +0100

    one more change challenge 2

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -14,12 +14,11 @@ JOIN authors ON authors.au_id = titleauthor.au_id;
 SELECT authors.au_id AS 'AUTHOR_ID',
 authors.au_lname AS 'LAST_NAME',
 authors.au_fname AS 'FIRST_NAME',
-titles.title AS 'TITLE',
 publishers.pub_name AS 'PUBLISHER',
 COUNT(titleauthor.title_id) AS 'TITLE_COUNT',
 SUM(COUNT(titleauthor.title_id)) OVER() AS 'TOTAL_COUNT'
-FROM titles
-JOIN publishers ON publishers.pub_id = titles.pub_id
+FROM publishers
+JOIN titles ON publishers.pub_id = titles.pub_id
 JOIN titleauthor ON titleauthor.title_id = titles.title_id
 JOIN authors ON authors.au_id = titleauthor.au_id
 GROUP BY AUTHOR_ID
